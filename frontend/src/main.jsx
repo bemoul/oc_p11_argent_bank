00a021d5826ec08f1a6aebf51729eb0e3ee59497
Selim-Ramdani <selimcodeur@gmail.com>
2024-01-20 12:02:11 +0100

    Add router : createBrowserRouter and RouterProvideer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Style import
+import "./assets/style/main.scss";
+
+// Pages import
+import {App} from './App.jsx'
+import { Home } from './pages/Home/Home.jsx';
+import { SignIn } from './pages/SignIn/SignIn.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <SignIn />
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
