4c419b0358aa2613884e2eae856bb008038e7298
Selim-Ramdani <selimcodeur@gmail.com>
2024-06-08 16:13:10 +0200

    change style for forms

--- frontend/src/components/UserEdit/UserEdit.jsx
+++ frontend/src/components/UserEdit/UserEdit.jsx
@@ -1,7 +1,16 @@
-import { useDispatch, useSelector } from "react-redux";
-import { profilEdit } from "../../reducers/authSlice";
-import { userUpdateProfile } from "../../reducers/authThunks";
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { profilEdit } from '../../reducers/authSlice';
+import { userUpdateProfile } from '../../reducers/authThunks';
 
+/**
+ * UserEdit component for editing user's profile
+ *
+ * @param {Object} props - The component props
+ * @param {string} props.firstname - The user's first name
+ * @param {string} props.lastname - The user's last name
+ * @returns {JSX.Element} The UserEdit component
+ */
 function UserEdit({ firstname, lastname }) {
   const { currentUser } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -27,10 +36,19 @@ function UserEdit({ firstname, lastname }) {
 
   return (
     <div className="user-edit">
-      <input className="user-edit-input" id="firstname" type="text" defaultValue={firstname} />
-      <input className="user-edit-input" id="lastname" type="text" defaultValue={lastname} />
-      <button className="save-btn" onClick={handleSaveEdit}>Save</button>
-      <button className="cancel-btn" onClick={handleCancelEdit}>Cancel</button>
+      <h2>Edit user info</h2>
+      <div className="form-group">
+        <label htmlFor="firstname">First name:</label>
+        <input className="user-edit-input" id="firstname" type="text" defaultValue={firstname} />
+      </div>
+      <div className="form-group">
+        <label htmlFor="lastname">Last name:</label>
+        <input className="user-edit-input" id="lastname" type="text" defaultValue={lastname} />
+      </div>
+      <div className="button-group">
+        <button className="save-btn" onClick={handleSaveEdit}>Save</button>
+        <button className="cancel-btn" onClick={handleCancelEdit}>Cancel</button>
+      </div>
     </div>
   );
 }
