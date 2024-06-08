b14fff99f687b18f201432be5f62c52c44361f83
Selim-Ramdani <selimcodeur@gmail.com>
2024-06-08 13:43:06 +0200

    Comments UserEdit

--- frontend/src/components/UserEdit/UserEdit.jsx
+++ frontend/src/components/UserEdit/UserEdit.jsx
@@ -1,48 +1,36 @@
 import { useDispatch, useSelector } from "react-redux";
-import { storeSelector } from "../../store/storeSelectors";
+import { profilEdit, userUpdateProfile } from "../../reducers/auth";
+function UserEdit({ firstname, lastname }) {
+  const { currentUser } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-/**
- * Component which displays inputs
- * for user profil update
- * 
- * @param {String} firstname 
- * @param {String} lastname 
- * 
- * @returns UserEdit component
- */
-function UserEdit({firstname, lastname}) {
-  const { currentUser, firstName, lastName } = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
+  const handleCancelEdit = (e) => {
+    e.preventDefault();
+    dispatch(profilEdit());
+  };
 
-    const handleCancelEdit = (e) => {
-        e.preventDefault();
+  const handleSaveEdit = (e) => {
+    e.preventDefault();
+    let token = currentUser;
+    console.log("token !!!!", token);
+    let newFirstName = document.querySelector("#firstname").value;
+    let newLastName = document.querySelector("#lastname").value;
 
-        dispatch(profilEdit());
-    }
+    console.log("Save button clicked");
+    console.log("New first name:", newFirstName);
+    console.log("New last name:", newLastName);
 
-    const handleSaveEdit = (e) => {
-        e.preventDefault();
+    dispatch(userUpdateProfile({ newFirstName, newLastName, token }));
+  };
 
-        let token = currentUser.authToken;
-        let newFirstName = document.querySelector("#firstname").value;
-        let newLastName = document.querySelector("#lastname").value;
-
-        console.log("Save button clicked");
-        console.log("New first name:", newFirstName);
-        console.log("New last name:", newLastName);
-
-        dispatch(userUpdateProfile({newFirstName, newLastName, token}));
-    }
-
-    return (
-        <div className="user-edit">
-            <input className="user-edit-input" id="firstname" type="text" defaultValue={firstname} />
-            <input className="user-edit-input" id="lastname" type="text" defaultValue={lastname} />
-            <button className="save-btn" onClick={(e) => handleSaveEdit(e)}>Save</button>
-            <button className="cancel-btn" onClick={(e) => handleCancelEdit(e)}>Cancel</button>
-        </div>
-    )
+  return (
+    <div className="user-edit">
+      <input className="user-edit-input" id="firstname" type="text" defaultValue={firstname} />
+      <input className="user-edit-input" id="lastname" type="text" defaultValue={lastname} />
+      <button className="save-btn" onClick={handleSaveEdit}>Save</button>
+      <button className="cancel-btn" onClick={handleCancelEdit}>Cancel</button>
+    </div>
+  );
 }
 
 export default UserEdit;
-
